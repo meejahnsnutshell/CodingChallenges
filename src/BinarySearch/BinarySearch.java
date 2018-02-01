ea28a27552be8cc788d07c1258a9916864a8ecff
Meg <the_meej@yahoo.com>
2018-02-01 16:17:36 -0500

    simplified code

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -1,74 +1,66 @@
 package BinarySearch;
 
-/**
- * A function that uses binary search steps to find the index of a given element within a sorted array.
- */
-
 public class BinarySearch {
-
-    public static int findIndex(int[] arr, int target){
-        // this is what we're searching for, initialized to -1
+    /**
+     * Takes an array and target element. Starting with the middle of the array, method eliminates half the array from
+     * search based on whether target is less than or greater than the middle element.
+     * @param arr - sorted array
+     * @param target - the element to be found
+     * @return targetIndex - the array index of the target element
+     */
+    public static int findIndex(int[] arr, int target) {
+        // this is what we're searching for, -1 indicates element not found
         int targetIndex = -1;
-        // boolean test if we've found the target
-        //boolean isTarget = false;
-        // get array length
         int arrLength = arr.length;
-        // if there's only 1 element
-        if (arrLength == 1){
+        // case of only 1 element in array
+        if (arrLength == 1) {
             // just check that element
-            if (arr[0] == target){
+            if (arr[0] == target) {
+                // target found, so assign index to targetIndex
                 targetIndex = 0;
-                System.out.println("We found the target of " + target + " at Index " + targetIndex + " with value " + arr[0]);
-                //isTarget = true;
-                return targetIndex;
-            } else {
-                System.out.println("The target of " + target + " is not in the array");
             }
-            // for any array size > 1
         } else {
-            // get the middle index
-            int midIndex = (arrLength-1)/2;
-            // get the element at midIndex
+            // for any array size > 1, find the middle index
+            int midIndex = (arrLength - 1) / 2;
+            // get the element at the middle index
             int midElement = arr[midIndex];
-            // check if midElement equals target
-            if (midElement == target){
-                //isTarget = true;
+            // check if midElement is the target
+            if (midElement == target) {
                 targetIndex = midIndex;
-                System.out.println("We found the target of " + target + " at Index " + midIndex + " with value " + arr[midIndex]);
-                // iterate down the array to arrLength-1 if midElement is less than target
-                return targetIndex;
-            } else if (target > midElement){
-                for (int i = midIndex + 1; i <= arrLength - 1; i++){
-                    if (arr[i] == target){
+            } else if (target > midElement) {
+                // iterate down the array to the end if midElement is less than target
+                for (int i = midIndex + 1; i <= arrLength - 1; i++) {
+                    if (arr[i] == target) {
                         targetIndex = i;
-                        System.out.println("We found the target of " + target + " at Index " + targetIndex + " with value " + arr[i]);
-                        return targetIndex;
+                        break;
                     }
                 }
                 //iterate up the array to 0 if target < midElement
             } else {
-                for (int i = midIndex - 1; i >= 0; i--){
-                    if (arr[i] == target){
+                for (int i = midIndex - 1; i >= 0; i--) {
+                    if (arr[i] == target) {
                         targetIndex = i;
-                        System.out.println("We found the target of " + target + " at Index " + targetIndex + " with value " + arr[i]);
-                        return targetIndex;
+                        break;
                     }
                 }
             }
         }
+
         // case where target isn't in the array, and targetIndex was never changed from initial value
-        if (targetIndex == -1){
+        if (targetIndex == -1) {
             System.out.println("The target of " + target + " is not in the array.");
+        } else {
+            System.out.println("We found the target of " + target + " at Index " + targetIndex + " with value " + arr[targetIndex]);
         }
         return targetIndex;
     }
 
     public static void main(String[] args) {
         // provide a sorted array
-        int[] arr = {3,5,17,20,25,26,33,1006,4500,4501};
+        int[] arr = {3, 5, 7, 9, 15, 17, 25};
 
         // element to be found
-        int target = 9;
+        int target = 25;
 
         findIndex(arr, target);
     }

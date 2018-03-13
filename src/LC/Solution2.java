93113b9ace0bb480c5e72ef5865522f2603203a3
Meg <the_meej@yahoo.com>
2018-03-13 11:01:28 -0400

    find first open spot in parking lot

--- src/LC/Solution2.java
+++ src/LC/Solution2.java
@@ -1,5 +1,53 @@
 package LC;
 
+/**
+ * LaunchCode challenge to write a program that returns the first available spot in a parking lot. 1's are occupied,
+ * 0's are empty parking spaces. Instructions stated that the next available spot should be furthest to the left that
+ * is also closest to the top. For example
+ * 1 1 1 1 0
+ * 0 1 1 1 1
+ * should return 1, 0
+ */
 public class Solution2 {
+    static int[] carParking(int n, int[][] available) {
+
+        int[] result = {-1, -1};
+        // iterate through array (of 0s and 1s)
+        // stop when first 0 is found in each row
+        // result[] is row and index of each 0 for each row
+        // return row & space of where car should be parked
+
+        for (int i = 0; i < n; i++) {   //column iteration
+            for (int j = 0; j < n; j++) {   //row iteration
+                if (available[j][i] == 0) { // look for zeros down each column
+                    result[0] = j;
+                    result[1] = i;
+                    System.out.print("A spot is available at ");
+                    for (int t: result) {
+                        System.out.print(t + " ");
+                    }
+                    return result;
+                }
+            }
+        }
+
+        if (result[0] == -1) {
+            System.out.println("The parking lot is full!");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] test = {
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 0}
+        };
+
+        carParking(5, test);
+    }
+
 
 }
